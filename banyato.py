60248298539e2c46a89bce9cc15778648de4724d
csucsa <csukodicsaba12316@gmail.com>
2023-03-20 09:24:02 +0100

    3. feladat kesz

--- banyato.py
+++ banyato.py
@@ -25,3 +25,22 @@ for melyseg_sor in melysegek:
             print(f"{Back.RESET}{Fore.BLACK}{melyseg:2d}", end=" ")
     print()
 
+print("2. feladat")
+be_sor=int(input("A mérés sorának azonosítója= ") or "12")
+be_oszlop=int(input("A mérés oszlopának azonosítója= ") or "6")
+be_melyseg=input(f"A mért mélység az adott helyen {melysegek[be_sor-1][be_oszlop-1]} dm")
+
+print("3. feladat")
+
+def megszamolas(m):
+    darab=0
+    for seged_lista in m:
+        for elem in seged_lista:
+            if elem>0:
+                darab+=1
+    return darab
+
+
+atlagos_melyseg=0
+
+print(f"A tó felszíne: {megszamolas(melysegek)*1} m2, átlagos mélysége: {atlagos_melyseg} m")
